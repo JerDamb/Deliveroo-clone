a2113c9b759ec5c9ecf36b89e78ccc52da79be4c
JerDamb <gaufre.mr@gmail.com>
2022-09-05 19:36:01 +0200

    ajout de la SafeArea sur android et mise en place du projet de bas

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,32 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React, { useLayoutEffect } from "react";
+import GlobalStyles from "../GlobalStyles";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    //masque la barre de navigation
+    navigation.setOptions({
+      headerShown: false,
+      headerTitle: "CLONE DE DELIVEROO",
+    });
+  }, []);
+
   return (
-    <View>
+    <SafeAreaView style={GlobalStyles.droidSafeArea}>
       <Text className="text-red-500">HomeScreen</Text>
-    </View>
+      {/* header */}
+      <View>
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
