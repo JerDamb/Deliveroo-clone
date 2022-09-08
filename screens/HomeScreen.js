cd918c3d6ce15d086787ee7a8ad0214ba65ab383
JerDamb <gaufre.mr@gmail.com>
2022-09-08 14:52:23 +0200

    add top of the app with searchbar, bugs with icons from heroicons pack

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,22 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  Image,
+  Text,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import React, { useLayoutEffect } from "react";
 import GlobalStyles from "../GlobalStyles";
+import {
+  UserIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+  ChevronDownIcon,
+} from "react-native-heroicons/outline";
+import { SearchBar } from "react-native-screens";
+import Categories from "../components/categories";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -15,17 +30,42 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView style={GlobalStyles.droidSafeArea}>
-      <Text className="text-red-500">HomeScreen</Text>
+    <SafeAreaView className="bg-white pt-5" style={GlobalStyles.droidSafeArea}>
       {/* header */}
-      <View>
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 p-2">
         <Image
           source={{
             uri: "https://links.papareact.com/wru",
           }}
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver now!</Text>
+          <Text className="font-bold text-xl">
+            Current location
+            {/* <Icons.ChevronDownIcon size={20} color="#00CCBB" /> */}
+          </Text>
+        </View>
+        {/* <Icons.UserIcon size={35} color="#00CCBB" /> */}
       </View>
+
+      {/*Search*/}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+          {/* <MagnifyingGlassIcon color="gray" size={20} /> */}
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+          {/* <Icons.AdjustmentsVerticalIcon color="#00CCBB" /> */}
+        </View>
+      </View>
+      {/* body */}
+      <ScrollView className="bg-gray-50">
+        {/* categories */}
+        <Categories />
+        {/* featured rows */}
+      </ScrollView>
     </SafeAreaView>
   );
 };
