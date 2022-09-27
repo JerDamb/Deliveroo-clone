8e3b83556ac1fc31459cc67054be20fa222038ee
JerDamb <gaufre.mr@gmail.com>
2022-09-27 15:29:25 +0200

    add sanity link and configuration

--- sanity/schemas/restaurant.js
+++ sanity/schemas/restaurant.js
@@ -6,40 +6,40 @@ export default {
     {
       name: "name",
       type: "string",
-      title: "title",
+      title: "Restaurant name",
       validation: (Rule) => Rule.required(),
     },
     {
       name: "short_description",
       type: "string",
-      title: "title",
+      title: "Short description",
       validation: (Rule) => Rule.max(200),
     },
     {
       name: "image",
       type: "image",
-      title: "title",
+      title: "Image of the Restaurant",
     },
     {
       name: "lat",
       type: "number",
-      title: "title",
+      title: "Latitude",
     },
     {
       name: "long",
       type: "number",
-      title: "title",
+      title: "Longitude",
     },
     {
       name: "address",
       type: "string",
-      title: "title",
+      title: "Restaurant adress",
       validation: (Rule) => Rule.required(),
     },
     {
       name: "rating",
       type: "number",
-      title: "title",
+      title: "Enter a rating from 1 to 5 stars",
       validation: (Rule) =>
         Rule.required()
           .min(1)
@@ -49,6 +49,7 @@ export default {
     {
       name: "type",
       type: "Category",
+      title: "Category",
       validation: (Rule) => Rule.required(),
       type: "reference",
       to: [{ type: "category" }],
--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,16 +5,12 @@ import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
 // We import object and document schemas
-import blockContent from "./blockContent";
-import category from "./category";
 import restaurant from "./restaurant";
-import author from "./author";
+import dish from "./dish";
+import featured from "./featured";
+import category from "./category";
 
-// Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
   name: "default",
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([restaurant, author, category, blockContent]),
+  types: schemaTypes.concat([restaurant, category, dish, featured]),
 });
